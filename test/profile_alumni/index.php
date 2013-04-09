919a7d54823ffa3129cba093ae0b5297f60e9888
Plaiplaipluz <suchada.plai@gmail.com>
2013-04-09 18:28:36 +0700

    pic menu

---
+++ test/profile_alumni/index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="th"><head>
+    <meta charset="utf-8">
+    <title>Bootstrap, from Twitter</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+      .sidebar-nav {
+        padding: 9px 0;
+      }
+
+      @media (max-width: 980px) {
+        /* Enable use of floated navbar text */
+        .navbar-text.pull-right {
+          float: none;
+          padding-left: 5px;
+          padding-right: 5px;
+        }
+      }
+    </style>
+    <link href="../css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="../css/design.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+  <script type="text/javascript" src="chrome-extension://bfbmjmiodbnnpllbbbfblcplfjjepjdn/js/injected.js"></script>
+
+  <script>
+    function form1(){
+      document.getElementById("form1").hidden = false;      
+      document.getElementById("form2").hidden = true;
+
+    }
+
+    function form2(){
+      document.getElementById("form1").hidden = true;      
+      document.getElementById("form2").hidden = false;
+
+    }
+  </script>
+</head>
+<body>
+
+<div class="container "> <!-- hearder profile-->
+	<div class="row-fluid profile_h0">
+      	<div class="span3">1</div>
+        <div class="span6">1</div>
+        <div class="span3">1</div>
+    </div>   
+    </div><!-- hearder profile-->
+    
+    <div class="container"> <!-- body profile-->
+    <div class="container-fluid">
+    <div class="row-fluid">
+    	<div class="span3">ssssssssss</div>
+        <div class="span9">
+        	<div class="container">
+           	 <div class="row-fluid">
+            	<div class="span3"></div>
+                <div class="span9"></div>
+           	</div>
+            </div>
+        </div>
+       </div>
+    </div>
+</div>
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+<!--    <script src="js/jquery.js"></script> 
+    
+    <script src="../../../../Users/Admin-Asus/js/bootstrap-transition.js"></script>
+    <script src="../../../../Users/Admin-Asus/js/bootstrap-alert.js"></script>
+    <script src="../../../../Users/Admin-Asus/js/bootstrap-modal.js"></script>
+    <script src="../../../../Users/Admin-Asus/js/bootstrap-dropdown.js"></script>
+    <script src="../../../../Users/Admin-Asus/js/bootstrap-scrollspy.js"></script>
+    <script src="../../../../Users/Admin-Asus/js/bootstrap-tab.js"></script>
+    <script src="../../../../Users/Admin-Asus/js/bootstrap-tooltip.js"></script>
+    <script src="../../../../Users/Admin-Asus/js/bootstrap-popover.js"></script>
+    <script src="../../../../Users/Admin-Asus/js/bootstrap-button.js"></script>
+    <script src="../../../../Users/Admin-Asus/js/bootstrap-collapse.js"></script>
+    <script src="../../../../Users/Admin-Asus/js/bootstrap-carousel.js"></script>
+    <script src="../../../../Users/Admin-Asus/js/bootstrap-typeahead.js"></script>-->
+    <script src="../js/jquery-1.9.1.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/bootstrap.js"></script>
+ 
+
+</body></html>
